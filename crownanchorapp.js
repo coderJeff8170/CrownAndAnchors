999ecfa4deb48511a4db0c54ac53e64e21e3a95f
Jeff Andrews <jeff@paintedbyjeff.com>
2019-04-22 07:23:44 -0400

    home 0723 042219

--- crownanchorapp.js
+++ crownanchorapp.js
@@ -11,6 +11,7 @@ var myFace = new Face('Crown', 10, 'imageurl');// you gotta use the new keyword 
 //if the var is greyed out, that means it's not being used (variable defined but never read)
 
 var myBet = new Bet(20, 'Spade');
+//you have to populate this from the DOM 
 var myBetAgain = new Bet('Crown', 10);
 
 console.log(`I'm betting ${myBet.betValue}`);
@@ -20,13 +21,3 @@ console.log(myBetAgain);
 catch(err) {
     console.log(err.message);
 }
-
-
-var badBet = new Bet('pumpkin', 'cumquot');
-
-
-
-
-var badFace = new Face('bad', 'bad');
-
-console.log
